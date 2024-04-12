cc79aadceb0296826aa68691b6961d4f48bf37ad
Zhihang Yao <zyao@vc.systems>
2024-04-12 07:46:37 +0000

    implement coordinate search

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { PluginConfigEditor, VcsPlugin, VcsUiApp } from '@vcmap/ui';
+import { Component } from 'vue';
+import { name, version, mapVersion } from '../package.json';
+import CoordinateSearch, { PluginConfig } from './coordinateSearch.js';
+import ConfigEditor from './ConfigEditor.vue';
+
+export default function coordinateSearchPlugin(
+  config: PluginConfig,
+): VcsPlugin<Record<never, never>, Record<never, never>> {
+  return {
+    get name(): string {
+      return name;
+    },
+    get version(): string {
+      return version;
+    },
+    get mapVersion(): string {
+      return mapVersion;
+    },
+    toJSON(): PluginConfig {
+      return config;
+    },
+    initialize(vcsUiApp: VcsUiApp): void {
+      vcsUiApp.search.add(new CoordinateSearch(vcsUiApp, config), name);
+    },
+    getDefaultOptions(): PluginConfig {
+      return {};
+    },
+    i18n: {
+      en: {
+        searchCoordinate: {
+          balloon: {
+            title: 'Searched Position',
+            description: 'Coordinate',
+          },
+          configEditor: {
+            projections: 'Projections',
+            addProjection: 'Add',
+            removeProjection: 'remove projection',
+            invalidInput: 'Please enter a valid epsg or proj4',
+          },
+        },
+      },
+      de: {
+        searchCoordinate: {
+          balloon: {
+            title: 'Gesuchte Position',
+            description: 'Koordinate',
+          },
+          configEditor: {
+            projections: 'Projektionen',
+            addProjection: 'hinzufügen',
+            removeProjection: 'Projektion entfernen',
+            invalidInput: 'Geben Sie gültige eine epsg oder proj4 ein',
+          },
+        },
+      },
+    },
+    getConfigEditors(): PluginConfigEditor[] {
+      return [{ component: ConfigEditor as Component & { title: string } }];
+    },
+  };
+}
